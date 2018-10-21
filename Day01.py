11e3abf8d8a8037d41486c832f39bb7e908200aa
mangeshjsm <mangesh.mohatkar@gmail.com>
2018-10-21 13:26:41 +0530

    this is first program

---
+++ Day01.py
@@ -0,0 +1,10 @@
+"""
+This is class one file
+ 1) we set up python3.7, git,pycharm
+ 2) Create Github repo for
+ 3)Created virtual environment
+ 4)Created pycharm project
+ 5) Write first program  Hello World
+"""
+
+print("Hello World")
