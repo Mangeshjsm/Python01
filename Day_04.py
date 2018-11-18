f77a9dc843e2ae8dd7f91f468b2b4ad92569391c
mangeshjsm <mangesh.mohatkar@gmail.com>
2018-11-18 10:05:22 +0530

    For Assignment of Day 4

--- Day_04.py
+++ Day_04.py
@@ -167,10 +167,12 @@ for i in range(5, 0, -1):
         print(j%2, end=" ")
     print("")
 
-"""
-
 
 for i in range(5, 0, -1):
     for j in range(i, 0, -1):
         print(j%2, end=" ")
     print("")
+
+"""
+
+
