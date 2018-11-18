75919e5446b12a92065c6712faf172f056dcaf2e
mangeshjsm <mangesh.mohatkar@gmail.com>
2018-11-18 17:54:42 +0530

    For Assignment of Day 5

--- Day_04.py
+++ Day_04.py
@@ -26,8 +26,7 @@ for i in range(0,5):
 for i in range(0,5):
 for j in range(0,5)
     
-"""
-"""
+
     i=0
     while True:
         if i<=5:
@@ -35,15 +34,12 @@ for j in range(0,5)
             break
             i +=1
 
-"""
 
-"""
 no = 1
 while no <= 5:
     print("*")
     no += 1
 
-"""
 
 for i in range(0, 5):
     for j in range(0, 5):
@@ -58,3 +54,146 @@ for i in range(0, 5):
     print("\r")
 
 
+
+for i in range(5):
+    for j in range(5):
+        if (j==0):
+            print("*",end=" ")
+        else:
+            print("*",end=" ")
+     print("\n")
+
+
+for i in range(0, 5):
+    for j in range(0, 5):
+        print("1", end=' ')
+    print("\r")
+
+
+#For Assignment no 3
+for i in range(0, 5):
+    num=i%2
+    for j in range(0, 5):
+        print(num, end=" ")
+    print("\r")
+
+
+for i in range(0, 5):
+    for j in range(0, 5):
+        if(i%2==0):
+            print("0", end=" ")
+        else:
+            print("1", end=" ")
+    print("")
+
+
+#For Assignment no 5
+
+for i in range(0, 5):
+    for j in range(0, i+1):
+        if(j%2==0):
+            print("1", end=" ")
+        else:
+            print("0", end=" ")
+    print("")
+
+
+
+#For Assignment no 4
+
+for i in range(0, 5):
+    for j in range(0, i+1):
+        i -=1
+        if(i%2==0):
+            print("1", end=" ")
+        else:
+            print("0", end=" ")
+    print("")
+
+#For Assignment no 4
+num=0
+for i in range(0, 5):
+    for j in range(0, i+1):
+        print(num%2, end=" ")
+        num +=1
+    if i%2 == 1:
+        num +=1
+    print("")
+
+
+for i in range(1, 6):
+    for j in range(0, i+1):
+        if(i%2==0):
+            print("1", end=" ")
+        else:
+            print("0", end=" ")
+    print("")
+
+
+
+#For Assignment no 6
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(j, end=" ")
+    print("")
+
+
+
+#For Assignment no 7
+
+num=1
+for i in range(0, 5):
+    for j in range(0, 5):
+        print('{num:02d}'.format(num=num), end=" ")
+        num +=1
+    print("")
+
+
+#OR
+num=1
+for i in range(0, 5):
+    for j in range(0, 5):
+        print('{:02d}'.format(num), end=" ")
+        num +=1
+    print("")
+
+
+
+#For Assignment no 8
+
+for i in range(5, 0, -1):
+    for j in range(i, 0, -1):
+        print(j%2, end=" ")
+    print("")
+
+
+for i in range(5, 0, -1):
+    for j in range(i, 0, -1):
+        print(j%2, end=" ")
+    print("")
+
+
+# for assignment no 12
+
+sum=2
+for i in range(0, 6):
+    for j in range(0, i+1):
+        if(j==0):
+            print("1", end=" ")
+        else:
+            print("0", end=" ")
+    print("")
+
+"""
+
+#ctrl+shift+f to format the python script
+
+sum = 2
+for i in range(0, 6):
+    for j in range(0, i + 1):
+        if (j == 0):
+            print("1", end=" ")
+        else:
+            print("0", end=" ")
+print("")
