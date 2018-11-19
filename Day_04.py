b93b48e7256c97a0e849858be324395ff74c7af2
mangeshjsm <mangesh.mohatkar@gmail.com>
2018-11-19 17:24:34 +0530

    For Assignment of Day 5

--- Day_04.py
+++ Day_04.py
@@ -192,7 +192,7 @@ for i in range(0, 6):
 
 for i in range(0, 6):
     power_of = 1
-    power_of = power_of * 2
+    power_of *= 2
     for j in range(0, i + 1):
         print(power_of, end=" ")
         power_of *= 2
